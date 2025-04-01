8b6b3bcf4b1b303799bcc0f7a0b1b560d1b28d4d
noel-pena <azulu88@hotmail.com>
2025-04-01 19:16:19 -0400

    refactored home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,101 +1,89 @@
 import { Link } from "react-scroll";
-import AppBar from "@mui/material/AppBar";
-import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import {
+    AppBar,
+    Link as MuiLink,
+    Stack,
+    Toolbar,
+    Typography,
+    Box,
+} from "@mui/material";
 import VerticalSplitIcon from "@mui/icons-material/VerticalSplit";
 
 export const Header = () => {
-  return (
-    <AppBar position="static" className="header">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <a href="/">
-            <VerticalSplitIcon
-              sx={{
-                display: { md: "flex" },
-                mr: 1,
-                color: "rgb(103, 131, 110, 0.7)",
-              }}
-            />
-          </a>
-          <a
-            href="/"
-            style={{
-              textDecoration: "none",
-              textDecorationColor: "none",
-              color: "white",
-            }}
-          >
-            <Typography
-              sx={{
-                display: { xs: "none", md: "flex" },
-              }}
-              variant="h7"
-              noWrap
-            >
-              JBC Professional LLC
-              <span
-                style={{
-                  fontFamily: "serif",
-                  fontWeight: 800,
-                  letterSpacing: ".1rem",
-                  color: "white",
-                  textDecoration: "none",
-                  "&:hover": {
-                    textDecoration: "underline",
-                    textDecorationColor: "#64485C",
-                    transition: "text-decoration-color 0.3s ease",
-                  },
-                }}
-              >
-                .
-              </span>
-            </Typography>
-          </a>
-          <div style={{ marginLeft: "auto" }}>
-            <Link
-              className="nav-items"
-              style={{
-                textDecoration: "none",
-                color: "white",
-                fontWeight: 300,
-              }}
-              to="services-section"
-              smooth={true}
-              duration={500}
-            >
-              Services
-            </Link>
-            <Link
-              className="nav-items"
-              style={{
-                paddingLeft: "20px",
-                textDecoration: "none",
-                color: "white",
-                fontWeight: 300,
-              }}
-              to="about-section"
-              smooth={true}
-              duration={500}
-            >
-              About
-            </Link>
-            <a
-              href="/gallery"
-              className="nav-items"
-              style={{
-                paddingLeft: "20px",
-                textDecoration: "none",
-                color: "white",
-                fontWeight: 300,
-              }}
-            >
-              Gallery
-            </a>
-          </div>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+    return (
+        <Stack>
+            <AppBar className="header" position="fixed">
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+                        <MuiLink href="/" sx={{ display: 'flex', alignItems: 'center' }}>
+                            <VerticalSplitIcon
+                                sx={{
+                                    pr: 1,
+                                    color: "rgb(103, 131, 110, 0.7)",
+                                }}
+                            />
+                        </MuiLink>
+                        <MuiLink
+                            href="/"
+                            sx={{
+                                textDecoration: "none",
+                                textDecorationColor: "none",
+                                color: "white",
+                                display: 'flex', alignItems: 'center' // Add this for vertical align
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    display: { xs: "none", md: "flex" },
+                                }}
+                                variant="h7"
+                                noWrap
+                            >
+                                JBC Professional LLC
+                            </Typography>
+                        </MuiLink>
+                    </Box>
+                    <Stack direction="row" gap={3}>
+                        <Link
+                            className="nav-items"
+                            sx={{
+                                textDecoration: "none",
+                                color: "white",
+                                fontWeight: 300,
+                            }}
+                            to="services-section"
+                            smooth={true}
+                            duration={500}
+                        >
+                            Services
+                        </Link>
+                        <Link
+                            className="nav-items"
+                            sx={{
+                                textDecoration: "none",
+                                color: "white",
+                                fontWeight: 300,
+                            }}
+                            to="about-section"
+                            smooth={true}
+                            duration={500}
+                        >
+                            About
+                        </Link>
+                        <MuiLink
+                            href="/gallery"
+                            className="nav-items"
+                            sx={{
+                                textDecoration: "none",
+                                color: "white",
+                                fontWeight: 300,
+                            }}
+                        >
+                            Gallery
+                        </MuiLink>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </Stack>
+    );
 };
